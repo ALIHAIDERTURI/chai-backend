42ab81e164390abc77c497123042b459957d75c9
alihaider <alihaiderturiofficial@gmail.com>
2024-08-08 01:22:30 +0500

    add user and video model

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-
+    
     app.on("error", (error)=>{
         console.log("ERROR:", error);
-        throw error  
+        throw error
+        
     })
-    
     app.listen(process.env.PORT || 8000, ()=>{
         
         console.log(`Server is running at port: ${process.env.PORT}`);
